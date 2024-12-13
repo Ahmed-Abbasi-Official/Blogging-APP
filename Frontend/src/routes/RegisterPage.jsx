a1a14d73e25dc1488c550fc95758d98176f33747
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2024-12-14 00:35:06 +0500

    Register

--- Frontend/src/routes/RegisterPage.jsx
+++ Frontend/src/routes/RegisterPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { SignUp } from "@clerk/clerk-react";
+import React from "react";
 
 const RegisterPage = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="flex justify-center items-center md:h-[calc(100vh-90px)]">
+      <SignUp signInUrl="/login"/>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
