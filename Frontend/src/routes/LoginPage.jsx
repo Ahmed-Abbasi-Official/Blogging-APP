e757a44620479e24d672f96dd0ee5fba5e65f4a3
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2024-12-14 00:34:53 +0500

    Login Page

--- Frontend/src/routes/LoginPage.jsx
+++ Frontend/src/routes/LoginPage.jsx
@@ -1,8 +1,11 @@
+import { SignIn } from '@clerk/clerk-react'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='flex items-center justify-center md:h-[calc(100vh-80px)] h-[calc(100vh-64px)] '>
+      <SignIn signUpUrl='/register' />
+    </div>
   )
 }
 
