d5f261aa6cc0830f6e5f47b5a6b1fd6ecd2758b7
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-09 16:35:40 +0500

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -18,21 +18,22 @@ const allowedOrigins = [process.env.AllowedOrigin1, process.env.AllowedOrigin2];
 // CORS configuration
 
 const corsOptions = {
-    origin: (origin, callback) => {
+  origin: (origin, callback) => {
       console.log("origin ==>", origin);
-      
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            console.error(`Blocked by CORS: ${origin}`);
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: ["GET, POST, PUT,'PATCH', DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    optionSuccessStatus: 200,
+
+      if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+      } else {
+          console.error(`Blocked by CORS: ${origin}`);
+          callback(new Error("Not allowed by CORS"));
+      }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Fixed here
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionSuccessStatus: 200,
 };
+
 
 app.use(cors(corsOptions));
 
