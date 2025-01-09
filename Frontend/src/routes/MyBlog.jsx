7b00b595470dd06014b944dd3682de2c2d9d078f
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-10 00:31:11 +0500

    commit

--- Frontend/src/routes/MyBlog.jsx
+++ Frontend/src/routes/MyBlog.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import PostListItem from "../components/PostListItem";
 import { useAuth } from "../context/userContext";
 
 const MyBlog = () => {
-  const[posts,setPosts]=useState([])
-    const {token}=useAuth();
-  const savedPosts = useQuery({
+  const { token } = useAuth();
+
+  const { data: posts, isLoading, isError, error } = useQuery({
     queryKey: ["savedPosts"],
     queryFn: async () => {
       const res = await axios.get(
@@ -18,48 +18,27 @@ const MyBlog = () => {
           },
         }
       );
-      //  console.log(res.data);
       return res.data;
     },
   });
-  
-  
 
-  if (savedPosts?.isPending) return <p>Loading...</p>;
-  if (savedPosts?.isError) return <p>Error: {savedPosts.error.message}</p>;
-  // console.log(savedPosts.savedPosts);
-  useEffect(()=>{
-    if(savedPosts?.data){
-      setPosts(savedPosts?.data)
-    }
-  },[savedPosts])
-  console.log(posts);
-  
-  
-  
-
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <>
-
-    {
-      savedPosts.isLoading?(
-        <p>Loading...</p>
-      ):(
-        posts.length===0 && (
-          <p className="text-center mb-6">No posts found.</p>
+      {
+        isLoading ?(
+          <p>Loading...</p>
+        ):(
+          posts?.length === 0 && (
+            <p className="text-center mb-6">No posts found.</p>
+          ) 
         )
-      )
-    }
-
-
-        
-            {posts.map((post) => (
-              <PostListItem key={post?._id} post={post} />
-            ))}
-      
-          
-  </>
+      }
+     { posts.map((post) => <PostListItem key={post._id} post={post} />)}
+    
+    </>
   );
 };
 
