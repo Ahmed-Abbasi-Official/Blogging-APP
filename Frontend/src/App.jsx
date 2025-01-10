1f494c5652e9def82e1deb70f06b2440bd3ab6db
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-10 19:26:52 +0500

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from 'react-router-dom'
 const App = () => {
+  
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 overflow-hidden">
       {/* NAVBAR */}
--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import {Outlet} from 'react-router-dom'
 import Navbar from "../components/Navbar";
+import Loader from "../components/Loader";
 
 const MainLayout = () => {
+  const [loader,setLoader]=useState(true)
+  setTimeout(()=>setLoader(false),2000)
+  console.log(loader);
+  
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-      <Navbar />
-      <Outlet />
+      {
+        loader ? (
+          <Loader/>
+        ):(
+          <>
+          <Navbar />
+          <Outlet />
+          </>
+        )
+      }
     </div>
   );
 };
